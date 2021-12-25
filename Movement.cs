0c30aff577556fdfeab724f3e9ebfbc5e90c4043
anouar zeklat <77289226+Imzigler@users.noreply.github.com>
2021-12-26 00:46:06 +0100

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float movementspeed;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+	    Vector3 move = new Vector3(Input.GetAxis("Horizontal")*movementspeed,0,Input.GetAxis("Vertical")*movementspeed).normalized;
+	    transform.Translate(move*movementspeed*Time.deltaTime,Space.World);
+	    if (move != Vector3.zero)
+	    {
+		    transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(move),movementspeed*Time.deltaTime);
+	    }
+	    if (Mathf.Abs(Input.GetAxis("Horizontal"))!= 0 || Mathf.Abs(Input.GetAxis("Vertical"))!=0)
+	    {
+	    	GetComponent<Animator>().SetBool("running",true);
+	    }
+	    else
+	    {
+	    	GetComponent<Animator>().SetBool("running",false);
+	    }
+    }
+}
