516edf6454fa7b40bb6d4cfb1a171995bb0398ec
anouar zeklat <77289226+Imzigler@users.noreply.github.com>
2021-12-26 00:47:59 +0100

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -11,12 +11,15 @@ public class Movement : MonoBehaviour
     }
     void Update()
     {
-	    Vector3 move = new Vector3(Input.GetAxis("Horizontal")*movementspeed,0,Input.GetAxis("Vertical")*movementspeed).normalized;
+    	    //movement
+	    Vector3 move = new Vector3(Input.GetAxis("Horizontal")*movementspeed,0,Input.GetAxis("Vertical")*movementspeed).normalized;	    
 	    transform.Translate(move*movementspeed*Time.deltaTime,Space.World);
+	    //rotation
 	    if (move != Vector3.zero)
 	    {
 		    transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(move),movementspeed*Time.deltaTime);
 	    }
+	    //animation
 	    if (Mathf.Abs(Input.GetAxis("Horizontal"))!= 0 || Mathf.Abs(Input.GetAxis("Vertical"))!=0)
 	    {
 	    	GetComponent<Animator>().SetBool("running",true);
